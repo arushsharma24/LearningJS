4abd383e236109ae22c5e0a467d441eb74ecd878
arushsharma24 <arush24astar@gmail.com>
2021-06-14 19:43:16 +0530

    practicing in the html file

---
+++ scopes.js
@@ -0,0 +1,24 @@
+console.log(x);
+var x = 5;
+absurd();
+console.log("mangoes are not mashed potatoes");
+//console.log(x);
+
+function absurd(){
+    console.log("mango porn should not exist");
+    console.log(x);
+    var x = 7;
+    console.log(x);
+}
+{
+    var x = 76;
+    console.log(x);
+}
+console.log(x);
+x = 69;
+{
+    console.log(x);
+    x = 32;
+    console.log(x);
+}
+console.log(x);
